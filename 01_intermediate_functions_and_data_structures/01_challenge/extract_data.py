9c37ea9a499965d089e651b66d50fddb8beae714
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-05-17 12:01:58 +0900

    Complete the challenge 01-01

--- 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
+++ 01_intermediate_functions_and_data_structures/01_challenge/extract_data.py
@@ -1,7 +1,17 @@
 import re
 
 def extract_data(text):
-    pass
+    patten = r"[a-zA-Z0-9]+:[a-zA-Z0-9]+"
+    return re.findall(patten, text)
 
 def better_extract_data(text):
-    pass
+    patten = r"([a-zA-Z0-9]+):([a-zA-Z0-9]+)"
+    result = re.findall(patten, text)
+    return result
+
+
+
+
+test_input = "The subject has Age:25 and Height:180cm.Other details are not relevant.Weight:70kg was noted."
+print(extract_data(test_input))
+print(better_extract_data(test_input))
