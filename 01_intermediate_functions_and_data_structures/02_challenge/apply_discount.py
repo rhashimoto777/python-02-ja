2fba3048170f361281958034f1d9db263106fcb3
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-05-17 12:25:28 +0900

    complete the challenge 01-02

--- 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
@@ -1,2 +1,25 @@
 def apply_discount(products, minimum_price, discount_rate):
-    pass
+    discount_coef = (100 - discount_rate)/100
+    a = lambda x : (x["name"], x["price"]*discount_coef)
+    b = [a(elem) for elem in products if elem["price"] >= minimum_price]
+    return b
+
+################ test case 1 ################
+products = [
+    {"name": "Laptop", "category": "Electronics", "price": 1200},
+    {"name": "Bread", "category": "Food", "price": 2},
+    {"name": "Jacket", "category": "Apparel", "price": 100}
+]
+minimum_price = 50
+discount_rate = 10
+print(apply_discount(products, minimum_price, discount_rate))
+
+################ test case 2 ################
+products = [
+    {"name": "Smartphone", "category": "Electronics", "price": 800},
+    {"name": "Sneakers", "category": "Footwear", "price": 120},
+    {"name": "Coffee", "category": "Beverage", "price": 5}
+]
+minimum_price = 100
+discount_rate = 15
+print(apply_discount(products, minimum_price, discount_rate))
